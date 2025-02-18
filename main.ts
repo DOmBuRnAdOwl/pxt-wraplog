66e4dda5b5f91bd7b29280a244635e7d045a2a8e
Dexter Latcham <latchamd@lancaster.ac.uk>
2025-02-18 07:53:13 +0000

    removing broken main file

--- main.ts
+++
@@ -1,6 +0,0 @@
-wraplogger.createBufferWithColumns("x")
-for (let index = 0; index < 4; index++) {
-    wraplogger.logData(wraplogger.createCV("x", 0))
-}
-wraplogger.saveBuffer()
-basic.showIcon(IconNames.Heart)
